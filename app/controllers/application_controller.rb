4932325dfcf635c8b4a76f3d841696d4b793e957
Ryan Bigg <radarlistener@gmail.com>
2010-04-08 03:29:45 +1000

    Add authorize_admin! filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+  def authorize_admin!
+    authorize_user!
+    unless current_user.admin?
+      flash[:notice] = "You must be an admin to do that."
+      redirect_to root_path
+    end
+  end 
 end
