bf9af8ac2fcd2137290fb9d7e12785a37e53b61b
Ryan Bigg <radarlistener@gmail.com>
2010-04-08 03:51:44 +1000

    Fix filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
   def authorize_admin!
-    authorize_user!
+    authenticate_user!
     unless current_user.admin?
       flash[:notice] = "You must be an admin to do that."
       redirect_to root_path
     end
-  end 
+  end
 end
--- cuke.rb
+++ cuke.rb
@@ -1,13 +1,31 @@
 #!/usr/bin/env ruby
-params = ARGV[0].split("").join("*")
-features = Dir["features/*#{params}*.feature"]
-if features.size == 1
-  puts "Running #{features.first}"
-  system("bundle exec cucumber #{features.first}")
-else
-  puts "Found multiple features:"
-  for feature in features
-    puts "  #{feature}"
-  end
-  puts "Please be more precise."
+def features_for(params)
+  Dir["features/*#{params}*.feature"]
 end
+
+def run(feature)
+  puts "Running #{feature}"
+  system("bundle exec cucumber #{feature}")
+end
+
+
+params = ARGV[0].split("").join("*")
+features = Dir["features/#{params}*.feature"]
+if features.size == 1
+  run(features.first)
+else
+  puts "Reading your mind..."
+  params = ARGV[0].split("").join("*_")
+  p params
+  features = features_for(params)
+  if features.size == 1
+    run(features.first)
+  else
+    puts "Found multiple features:"
+    for feature in features
+      puts "  #{feature}"
+    end
+    puts "Please be more precise."
+  end
+end
+
