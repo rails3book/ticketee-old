9b159c6d4faec6bf01ca758d3e5d8c648fd4c765
Ryan Bigg <radarlistener@gmail.com>
2010-04-08 02:32:08 +1000

    Story #12: Associate users with tickets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :tickets
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable, :confirmable,
