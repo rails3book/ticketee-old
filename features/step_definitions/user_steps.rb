fdb33e1806d9b244a88951d3c3fdadab5b43ad97
Ryan Bigg <radarlistener@gmail.com>
2010-04-08 00:28:15 +1000

    Define confirmation step

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,7 @@
 Given /^there is a user with the email address "([^\"]*)" and password "([^\"]*)"$/ do |email, password|
   User.create!(:email => email, :password => password, :password_confirmation => password)
 end
+
+Given /^"([^\"]*)" has confirmed their account$/ do |email|
+  User.find_by_email(email).confirm!
+end
