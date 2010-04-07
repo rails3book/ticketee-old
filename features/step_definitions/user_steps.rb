3834d0e38b4d5129ddcc7d6d8b007c40f3cf8a95
Ryan Bigg <radarlistener@gmail.com>
2010-04-08 02:54:13 +1000

    Story #12: Implement 'given I am logged in as them' step

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,7 +1,17 @@
 Given /^there is a user with the email address "([^\"]*)" and password "([^\"]*)"$/ do |email, password|
-  User.create!(:email => email, :password => password, :password_confirmation => password)
+  @user = User.create!(:email => email, :password => password, :password_confirmation => password)
 end
 
 Given /^"([^\"]*)" has confirmed their account$/ do |email|
   User.find_by_email(email).confirm!
+end
+Given /^I am logged in as them$/ do
+  steps(%Q{
+    Given I am on the homepage
+    When I follow "Login"
+    And I fill in "Email" with "#{@user.email}"
+    And I fill in "Password" with "#{@user.password}"
+    And I press "Sign in"
+    Then I should see "Signed in successfully."
+  })
 end
