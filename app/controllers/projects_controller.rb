5d95ef1f2065d110522bec54c0150255ae23ce73
Ryan Bigg <radarlistener@gmail.com>
2010-05-08 20:47:38 +1000

    Edited a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,4 +22,19 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:error] = "Project has not been updated."
+      render :action => "edit"
+    end
+  end
 end
