7131036ea3e71a49e9068750ef250fa1447dd572
Ryan Bigg <radarlistener@gmail.com>
2010-06-06 21:42:19 +1000

    Remove debug line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   layout 'application'
   def authorize_admin!
     authenticate_user!
-    puts current_user.admin?
     unless current_user.admin?
       flash[:alert] = "You must be an admin to do that."
       redirect_to root_path
