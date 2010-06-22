ebf3c7d13fcebb506102ff92dcae197188251edd
Ryan Bigg <radarlistener@gmail.com>
2010-06-23 00:02:16 +1000

    Remove duplicate migrations

--- db/migrate/20100530180417_add_user_id_to_tickets.rb
+++
@@ -1,9 +0,0 @@
-class AddUserIdToTickets < ActiveRecord::Migration
-  def self.up
-    add_column :tickets, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :tickets, :user_id
-  end
-end
