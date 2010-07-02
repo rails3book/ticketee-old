ed85abe3eb2b0f65c09f73a313375b7d7c556a05
Ryan Bigg <radarlistener@gmail.com>
2010-07-02 11:42:13 +1000

    Write tickets controller spec

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe TicketsController do
+  before do
+    stub_warden
+  end
 
+  let(:user) { User.create(:email => "user@ticktee.com", :password => "password", :password_confirmation => "password") }
+  let(:project) { Project.create(:name => "Ticketee") }
+  let(:ticket) { project.tickets.create(:title => "Restrict permissions", :description => "For the tickets controller.")}
+
+  context "standard users" do
+    it "cannot access a ticket for a project they don't have access to" do
+      get :show, :id => ticket.id, :project_id => project.id
+      response.should redirect_to(root_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
+  end
 end
