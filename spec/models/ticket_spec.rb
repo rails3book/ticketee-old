c6168a07e27f9733641f8972966fd80eb7a1be94
Ryan Bigg <radarlistener@gmail.com>
2010-07-20 21:22:50 +1000

    Duplicate bug that causes undefined method insert_record to be called.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Ticket do
+  # Works!
   it "creates nested assets" do
     ticket = Ticket.create(:title => "Assets galore!",
                            :description => "More than one!",
@@ -8,5 +9,16 @@ describe Ticket do
                                                    "1" => { :asset => File.open("spec/fixtures/spin.txt") }
                                                  }
                            )
+  end
+  
+  #Fails!
+  it "creates nested assets too" do
+    ticket = Ticket.new(:title => "Assets galore!",
+                           :description => "More than one!",
+                           :assets_attributes => { "0" => { :asset => File.open("spec/fixtures/speed.txt") },
+                                                   "1" => { :asset => File.open("spec/fixtures/spin.txt") }
+                                                 }
+                           )
+    ticket.save
   end
 end
