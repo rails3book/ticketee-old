624a452bc44440b65e81a209f66f12cdb934484b
Ryan Bigg <radarlistener@gmail.com>
2010-08-22 19:45:52 +1000

    Fail.

--- db/schema.rb
+++ db/schema.rb
@@ -1,8 +1,8 @@
-# This file is auto-generated from the current state of the database. Instead 
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your 
+# Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100814103201) do
+ActiveRecord::Schema.define(:version => 20100718123954) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -20,16 +20,6 @@ ActiveRecord::Schema.define(:version => 20100814103201) do
     t.integer  "ticket_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "comments", :force => true do |t|
-    t.text     "text"
-    t.integer  "ticket_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "previous_state_id"
-    t.integer  "state_id"
   end
 
   create_table "permissions", :force => true do |t|
@@ -47,10 +37,6 @@ ActiveRecord::Schema.define(:version => 20100814103201) do
     t.datetime "updated_at"
   end
 
-  create_table "states", :force => true do |t|
-    t.string "name"
-  end
-
   create_table "tickets", :force => true do |t|
     t.string   "title"
     t.text     "description"
@@ -59,7 +45,6 @@ ActiveRecord::Schema.define(:version => 20100814103201) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.datetime "asset_updated_at"
-    t.integer  "state_id"
   end
 
   create_table "users", :force => true do |t|
