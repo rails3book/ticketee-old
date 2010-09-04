de5c5336e5b03f4f73a69236419e8c3eb78dd369
Ryan Bigg <radarlistener@gmail.com>
2010-09-04 21:07:48 +1000

    We don't actually write this spec.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,13 +1,5 @@
 require 'spec_helper'
 
 describe Ticket do
-  it "creates nested assets" do
-    ticket = Ticket.create(:title => "Assets galore!",
-                           :description => "More than one!",
-                           :assets_attributes => { "0" => { :asset => File.open("spec/fixtures/speed.txt") },
-                                                   "1" => { :asset => File.open("spec/fixtures/spin.txt") }
-                                                 }
-                           )
-    ticket.assets.count.should eql(2)
-  end
+
 end
