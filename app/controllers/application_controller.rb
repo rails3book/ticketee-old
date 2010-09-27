8cf6fe4778685c97d96aa2787efab8277c5f09bb
Ryan Bigg <radarlistener@gmail.com>
2010-09-27 20:13:58 +1000

    Specify states as styles.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :find_states
   
   private
   
@@ -16,5 +18,9 @@ class ApplicationController < ActionController::Base
     rescue ActiveRecord::RecordNotFound
       flash[:alert] = "The project you were looking for could not be found."
       redirect_to root_path
+  end
+
+  def find_states
+    @states = State.all
   end
 end
