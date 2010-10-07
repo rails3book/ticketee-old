0c2b01261d732a63bff7c9be7c723754709c5360
Ryan Bigg <radarlistener@gmail.com>
2010-10-08 08:58:16 +1100

    Clicking on a tag now goes to that tag's "search" page.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,9 @@ class CommentsController < ApplicationController
     
     @comment = @ticket.comments.build(params[:comment].merge(:user => current_user))
     if @comment.save
-      @ticket.tag!(params[:tags]) if can?(:tag, @ticket.project)
+      if can?(:tag, @ticket.project) || current_user.admin?
+        @ticket.tag!(params[:tags])
+      end
       flash[:notice] = "Comment has been created."
       redirect_to [@ticket.project, @ticket]
     else
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def remove
     @ticket = Ticket.find(params[:ticket_id])
-    if can?(:tag, @ticket.project)
+    if can?(:tag, @ticket.project) || current_user.admin?
       @tag = Tag.find(params[:id])
       @ticket.tags -= [@tag]
       @ticket.save
