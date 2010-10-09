e21c738fc133029d283324819f50e35d9a5dd6d2
Ryan Bigg <radarlistener@gmail.com>
2010-10-09 12:55:41 +1100

    Add config.action_mailer setting to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Ticketee
   class Application < Rails::Application
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
