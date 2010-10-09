d54f32ce7693449418ff23d220c951f4430f9002
Ryan Bigg <radarlistener@gmail.com>
2010-10-09 12:10:24 +1100

    WIP- Use 'Given there are the following users step'

--- features/creating_projects.feature
+++ features/creating_projects.feature
@@ -4,7 +4,10 @@ Feature: Creating projects
   I want to create them easily
 
   Background:
-    Given there is an admin with the email address "admin@ticketee.com" and password "password"
+    Given there are the following users:
+      | email              | password | admin |
+      | admin@ticketee.com | password | true  |
+      
     And I am signed in as them
     Given I am on the homepage
     When I follow "New Project"
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,8 +1,12 @@
-Given /^there is an? (unconfirmed)?\s?(admin|user) with the email address "([^\"]*)" and password "([^\"]*)"$/ do |unconfirmed, admin, email, password|
-  @user = User.new(:email => email, :password => password, :password_confirmation => password)
-  @user.admin = true if admin == "admin"
-  @user.save!
-  @user.confirm! unless unconfirmed
+Given /^there are the following users:$/ do |table|
+  table.hashes.each do |attributes|
+    @user = User.create!(
+      attributes.merge!(
+        :password_confirmation => attributes[:password]
+      )
+    )
+    @user.confirm!
+  end
 end
 
 Given /^"([^\"]*)" has not yet confirmed their account$/ do |email|
