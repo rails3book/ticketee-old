94a0714dca5be75ac685a6deea9ca7333f251185
Ryan Bigg <radarlistener@gmail.com>
2011-04-02 20:54:25 +1100

    Don't do let(token), just blank string it

--- spec/api/v1/authentication_spec.rb
+++ spec/api/v1/authentication_spec.rb
@@ -1,10 +1,9 @@
 require "spec_helper"
 
 describe "V1 Authentication", :type => :api do
-  let(:token) { "" }
 
   it "making a request with no token fails" do
-    get "/api/v1/projects.json", :token => token
+    get "/api/v1/projects.json", :token => ""
     error = { :error => "Token is invalid." }
     last_response.body.should eql(error.to_json)
   end
